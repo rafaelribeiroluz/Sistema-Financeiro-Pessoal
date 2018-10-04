e746a1daa6a6f063e3e113a137c07bdb572ea14a
rafaelribeiroluz <rafael.ribeiroluz@gmail.com>
2018-10-04 15:28:12 -0300

    Layout Tela Login

--- Sistema Financeiro/View/Windows/Login.xaml.cs
+++ Sistema Financeiro/View/Windows/Login.xaml.cs
@@ -23,5 +23,10 @@ namespace View
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
